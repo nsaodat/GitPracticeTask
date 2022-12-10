245b4b3d3662a45b62f54870aaa01cd33ec43a57
Nafisa Ravshanova <nafisa.ravshanova@outlook.com>
2022-12-10 16:41:38 +0000

    update 2 to Food file

--- src/Practice1/Food.java
+++ src/Practice1/Food.java
@@ -6,4 +6,6 @@ public class Food {
 
     //As many different factors influence the length of time that the body can last without food, this period will vary among individuals.
 
+    // Without food, the body starts to use its own tissue as fuel, but it can only do this for so long.
+
 }

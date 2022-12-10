6e66a8faccf8b86b0f964a1e2cd73c068f7e6790
Nafisa Ravshanova <nafisa.ravshanova@outlook.com>
2022-12-10 16:39:47 +0000

    update 1 to Food file

--- src/Practice1/Food.java
+++ src/Practice1/Food.java
@@ -4,4 +4,6 @@ public class Food {
 
     // In general, it is likely that a person could survive between 1 and 2 months without food.
 
+    //As many different factors influence the length of time that the body can last without food, this period will vary among individuals.
+
 }
